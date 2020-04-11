5b992cd04ddc137aa098dea7485847b2288a4f65
Zachary Murray <zacharymurray@hotmail.com>
2020-04-11 10:34:27 +1000

    Added mass to body class

--- src/models/Body.ts
+++ src/models/Body.ts
@@ -4,17 +4,21 @@ class Body {
     // Private fields
     private _mesh : Mesh;
     private _light : PointLight = null;
-    private _velocity: Vector3 = new Vector3(0, 0, 0);
+    
 
     // Public API
+    public mass: number;
+    public velocity: Vector3 = new Vector3(0, 0, 0);
     public get mesh() : Mesh { return this._mesh; }
     public get light() : PointLight { return this._light; }
     public get position() : Vector3 { return this._mesh.position; }
-    public get velocity() : Vector3 { return this._velocity; }
+    public set position(v: Vector3) { this._mesh.position = v; }
+    
 
-    constructor(mesh: Mesh, velocity: Vector3, light: PointLight = null) {
+    constructor(mesh: Mesh, mass: number, velocity: Vector3, light: PointLight = null) {
         this._mesh = mesh;
-        this._velocity = velocity;
+        this.velocity = velocity;
         this._light = light;
+        this.mass = mass;
     }
 }
