5eb3baf00a3bc24f0a2c05f8825bad8b8f2ba7b6
protango <31492239+protango@users.noreply.github.com>
2021-01-15 16:58:57 +1000

    Remove mapping file links and add .nojekyll file

--- build_pages_site.ps1
+++ build_pages_site.ps1
@@ -13,8 +13,13 @@ npm run build
 # copy build files to docs directory
 Copy-Item -Path "dist\public" -Destination "docs" -Recurse
 
+# add .nojekyll file to bypass GH pages jekyll build
+New-Item -Path .nojekyll
+
 # delete mapping files
 Remove-Item -Path "docs\*.map"
+((Get-Content -path docs\bundle.js -Raw) -replace "^//# sourceMappingURL=.*?\.map$","") | Set-Content -Path docs\bundle.js -NoNewline
+((Get-Content -path docs\app.css -Raw) -replace "^/\*# sourceMappingURL=.*?\.map\*/$","") | Set-Content -Path docs\app.css -NoNewline
 
 # restore webpack config
 Set-Content -Path webpack.config.js -Value $webpackconf -NoNewline
